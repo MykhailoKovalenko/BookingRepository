03ce75636f9e3355af8669b0e0e2bf4a2f88699c
MykhailoKovalenko <m.kovalenko2791@gmail.com>
2021-10-26 19:31:18 +0300

    changes in repo

---
+++ BookingRooms/Controllers/BookingController.cs
@@ -0,0 +1,37 @@
+﻿using BookingRooms.Interfaces;
+using BookingRooms.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingRooms.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingController : ControllerBase
+    {
+        private IBookingService _bookingService;
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Booking))]
+        [ProducesResponseType(400)]
+        public IActionResult Create([FromBody] Booking booking)
+        {
+            if (booking == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _bookingService.Add(booking);
+            return CreatedAtAction(nameof(Create), new { id = booking.Id }, booking);
+        }
+
+    }
+}
