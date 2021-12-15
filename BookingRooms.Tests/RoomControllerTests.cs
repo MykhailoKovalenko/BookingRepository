5f97bc9243cb3c2b531ec817deee7a389d79675d
MykhailoKovalenko <m.kovalenko2791@gmail.com>
2021-12-15 18:08:51 +0200

    Test impl

--- BookingRooms.Tests/RoomControllerTests.cs
+++ BookingRooms.Tests/RoomControllerTests.cs
@@ -6,6 +6,7 @@ using BookingRooms.Models;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using SharedBookingLibrary.DTO;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,15 +17,16 @@ namespace BookingRooms.Tests
 {
     public class RoomControllerTests
     {
+        private readonly Mock<IRoomService> serviceStub = new Mock<IRoomService>();
+        private readonly Mock<IMapper> mapperStub = new Mock<IMapper>();
+        private readonly Random rand = new Random();
+
         [Fact]
         public async Task GetRoom_UnexistingItem_NotFound()
         {
             // Arrange
-            var serviceStub = new Mock<IRoomService>();
             serviceStub.Setup(s => s.GetAsync(It.IsAny<int>()))
                 .ReturnsAsync((Room)null);
-
-            var mapperStub = new Mock<IMapper>();
 
             var controller = new RoomController(serviceStub.Object, mapperStub.Object);
 
@@ -33,7 +35,39 @@ namespace BookingRooms.Tests
 
             // Assert
             Assert.IsType<OkObjectResult>(result.Result);
-            typeof(RoomController).GetMethod(nameof(RoomController.GetRoom)).Should().BeDecoratedWith<AsyncActionFilterRoomIdValidation>();
+           // typeof(RoomController).GetMethod(nameof(RoomController.GetRoom)).Should().BeDecoratedWith<AsyncActionFilterRoomIdValidation>();
+        }
+
+        [Fact]
+        public async Task GetRoom_ExistingItem_ExpectedRoom()
+        {
+            // Arrange
+            var expectedRoom = CreateRandomRoom();
+
+            serviceStub.Setup(s => s.GetAsync(It.IsAny<int>()))
+                .ReturnsAsync(expectedRoom);
+
+            var controller = new RoomController(serviceStub.Object, mapperStub.Object);
+
+            // Act
+            var result = await controller.GetRoom(expectedRoom.Id);
+
+            // Assert
+            result.Value.Should().BeEquivalentTo(
+                expectedRoom,
+                options => options.ComparingByMembers<Room>()
+                );
+        }
+
+        private Room CreateRandomRoom()
+        {
+            return new Room
+            {
+                Id = rand.Next(1, 100),
+                Name = "test room",
+                Places = rand.Next(1, 100),
+                IsProjector = true
+            };
         }
     }
 }
